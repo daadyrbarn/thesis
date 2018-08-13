1297ab18e66cfb0445a631a97393f73a6a03a02a
daadyrbarn <daadyrbarn@gmail.com>
2018-08-13 21:39:48 +0200

    Write journal

--- test.py
+++ test.py
@@ -1,8 +1,17 @@
+## Take a list of values and calculate the average values.
+#  Also calculate difference between each value and the avg.
+#  Also also print the values as absolute (positive) numbers.
+
 numbers = [113, 200, 144, 99, 145]
 
 avg_num = sum(numbers)/len(numbers)
-
 print(avg_num)
 
-#for x in numbers:
-#    if x ==
+dev_list = []
+
+for x in numbers:
+    a = x - avg_num
+    dev_list.append(a)
+print(dev_list)
+
+print([abs(n) for n in dev_list])
